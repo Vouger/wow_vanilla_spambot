009d3fc79f0f25d937b008ae9c753686e66aadba
Roman Dehtiarov <rvougers@gmail.com>
2016-04-26 23:48:34 +0300

    publishing code

---
+++ vanilla_spam_bot/MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using HtmlAgilityPack;
+
+
+namespace VanillaSpamBot
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        public Thread thread;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (Process p in Process.GetProcessesByName("wow"))
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(p.MainWindowTitle))
+                    {
+                        var data = new Test { Test1 = p.MainWindowTitle, Test2 = p.Id.ToString() };
+
+                        dataGrid1.Items.Add(data);
+                    }
+                }
+                catch { }
+            }
+
+            
+        }
+
+        private void ButtonStartClick(object sender, RoutedEventArgs e)
+        {
+            Test t = (Test)dataGrid1.SelectedItem;
+            IntPtr hWnd_wow;
+            if (t != null)
+            {
+                hWnd_wow = Process.GetProcessById(Int32.Parse(t.Test2)).MainWindowHandle;
+            }
+            else
+            {
+                Process[] pr = Process.GetProcessesByName("wow");
+                hWnd_wow = Process.GetProcessById(pr[0].Id).MainWindowHandle;
+            }
+
+            string text = textMessage.Text;
+            int pause = Int32.Parse(pauseBox.Text);
+            int relog = Int32.Parse(relogBox.Text);
+
+            string side = "Horde";
+            label1.Content = "Horde nicknames";
+            if (radioAlliance.IsChecked == true)
+            {
+                side = "Alliance";
+                label1.Content = "Alliance nicknames";
+            }
+
+            string server = "NRB";
+            if (radioPve.IsChecked == true)
+            {
+                server = "NBE";
+            }
+
+            thread = new Thread(() => Spam(hWnd_wow, text, pause, relog, side, server));
+            thread.Start();
+
+        }
+
+        private void ButtonStopClick(object sender, RoutedEventArgs e)
+        {
+            thread.Abort();
+        }
+
+
+        private void ButtonGetNicknamesClick(object sender, RoutedEventArgs e)
+        {
+            GetPlayers();
+        }
+
+        private void GetPlayers() {
+            dataGrid2.Items.Clear(); ;
+            WebClient client = new WebClient();
+            for (int j = 1; j <= 25; j++)
+            {
+                string side = "Horde";
+                label1.Content = "Horde nicknames";
+                if (radioAlliance.IsChecked == true) {
+                   side = "Alliance";
+                   label1.Content = "Alliance nicknames";
+                }
+                string playersSite = client.DownloadString("http://realmplayers.com/CharacterList.aspx?search=&realm=NRB&race="+ side + "&sort=Seen&page="+j);
+               
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(playersSite);
+
+                foreach (var row in doc.DocumentNode.SelectNodes("//table[@class = 'table']/tbody/tr"))
+                {
+                    var player = new Player();
+                    int i = 0;
+                    foreach (var cell in row.SelectNodes("td"))
+                    {
+                        if (i == 0)
+                        {
+                            player.Rank = cell.InnerText;
+                        }
+
+                        if (i == 1)
+                        {
+                            player.Name = cell.InnerText;
+                        }
+
+                        if (i == 2)
+                        {
+                            player.Guild = cell.InnerText;
+                        }
+
+                        if (i == 3)
+                        {
+                            player.Level = cell.InnerText;
+                        }
+
+                        i++;
+                    }
+                    dataGrid2.Items.Add(player);
+
+                }
+                label1.Content += " " + dataGrid2.Items.Count;
+            }
+        }
+
+        private List<Player> GetPlayers2(string side, string server )
+        {
+            List<Player> players = new List<Player>();
+            WebClient client = new WebClient();
+            for (int j = 1; j <= 2; j++)
+            {
+                string playersSite = client.DownloadString("http://realmplayers.com/CharacterList.aspx?search=&realm="+ server + "&race=" + side + "&sort=Seen&page=" + j);
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(playersSite);
+
+                foreach (var row in doc.DocumentNode.SelectNodes("//table[@class = 'table']/tbody/tr"))
+                {
+                    var player = new Player();
+                    int i = 0;
+                    foreach (var cell in row.SelectNodes("td"))
+                    {
+                        if (i == 0)
+                        {
+                            player.Rank = cell.InnerText;
+                        }
+
+                        if (i == 1)
+                        {
+                            player.Name = cell.InnerText;
+                        }
+
+                        if (i == 2)
+                        {
+                            player.Guild = cell.InnerText;
+                        }
+
+                        if (i == 3)
+                        {
+                            player.Level = cell.InnerText;
+                        }
+
+                        i++;
+                    }
+                    players.Add(player);
+                }
+            }
+            return players;
+        }
+
+        public void Spam(IntPtr hWnd_wow, string text, int pause, int relog, string side, string server)
+        {
+            while (true) {
+                int i = 0;
+                List<Player> players = GetPlayers2(side, server);
+                foreach (Player row in players)
+                {
+                    int j = 0;
+                    var rnd = new Random(DateTime.Now.Millisecond);
+                    int ticks = rnd.Next(1000, pause * 1000);
+                    string name = row.Name;
+                    WoWSlashCommand.send(hWnd_wow, "/w " + name + " " + text + "      ("+ticks.ToString()+")");
+                    Thread.Sleep(ticks);
+                    if (i > 20) {
+                        WoWSlashCommand.send(hWnd_wow, "/logout");
+                        Thread.Sleep(relog * 1000);
+                        WoWSlashCommand.enter(hWnd_wow);
+                        Thread.Sleep(relog * 1000);
+                        i = 0;
+                    }
+                    i++;
+                    j++;
+                    if ((j > 100 && String.Compare(server, "NBE") == 0) || (j > 50 && String.Compare(server, "NRB") == 0))
+                        break;
+                }
+           }
+
+        }
+
+    }
+
+    public class Test
+    {
+        public string Test1 { get; set; }
+        public string Test2 { get; set; }
+    }
+
+    public class Player
+    {
+        public string Rank { get; set; }
+        public string Name { get; set; }
+        public string Guild { get; set; }
+        public string Level { get; set; }
+    }
+
+
+}
---
+++ vanilla_spam_bot/WoWSlashCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+
+namespace VanillaSpamBot
+{
+    class WoWSlashCommand
+    {
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr thWnd, int msg, int wParam, IntPtr lParam);
+
+        private const int WM_CHAR = 0x0102;
+        private const int VK_CONTROL = 0xA2;
+        private const int WM_KEYDOWN = 0x100;
+        private const int WM_KEYUP = 0x101;
+        private const int VK_RETURN = 0x0D;
+        private const int VK_ESCAPE = 0x1B;
+
+        public static void send(IntPtr hWnd, string slashCommand)
+        {
+            // Object savedClipboard = Clipboard.GetDataObject();
+            //Clipboard.SetText(slashCommand);
+
+            SendMessage(hWnd, WM_KEYDOWN, VK_ESCAPE, IntPtr.Zero);
+            SendMessage(hWnd, WM_KEYUP, VK_ESCAPE, IntPtr.Zero);
+
+            SendMessage(hWnd, WM_KEYDOWN, VK_RETURN, IntPtr.Zero);
+            SendMessage(hWnd, WM_KEYUP, VK_RETURN, IntPtr.Zero);
+            int i = 0;
+            foreach (char c in slashCommand)
+            {
+                IntPtr val = new IntPtr(c);
+                if(i<2)
+                    Thread.Sleep(200);
+                SendMessage(hWnd, WM_CHAR, c, IntPtr.Zero);
+                SendMessage(hWnd, WM_KEYDOWN, VK_CONTROL, IntPtr.Zero);
+                SendMessage(hWnd, WM_KEYDOWN, 0x56, IntPtr.Zero);
+                SendMessage(hWnd, WM_KEYUP, 0x56, IntPtr.Zero);
+                SendMessage(hWnd, WM_KEYUP, VK_CONTROL, IntPtr.Zero);
+                i++;
+            }
+            SendMessage(hWnd, WM_KEYDOWN, VK_RETURN, IntPtr.Zero);
+            SendMessage(hWnd, WM_KEYUP, VK_RETURN, IntPtr.Zero);
+        }
+
+        public static void enter(IntPtr hWnd)
+        {
+            SendMessage(hWnd, WM_KEYDOWN, VK_RETURN, IntPtr.Zero);
+            SendMessage(hWnd, WM_KEYUP, VK_RETURN, IntPtr.Zero);
+        }
+    }
+}
